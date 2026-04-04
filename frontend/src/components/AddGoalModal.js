10eb3f64c12d56946b5a8b54814cf787979d3afb
JeyashreeBalakrishnan <jeyashree1905@gmail.com>
2026-04-04 21:24:43 +0530

    Hardcode backend URL for goals

--- frontend/src/components/AddGoalModal.js
+++ frontend/src/components/AddGoalModal.js
@@ -45,7 +45,7 @@ const AddGoalModal = ({ isOpen, onClose, onGoalAdded, editingGoal }) => {
     //const res = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/goals/add`, goalData);
     //const res = await axios.post('http://localhost:5000/api/goals/add', goalData);
     // Add this line
-     const res = await axios.post('https://talentflow-1eyr.onrender.com/api/goals/add', newGoal);
+     const res = await axios.post('https://talentflow-1eyr.onrender.com/api/goals/add', goalData);
     if (res.status === 201) {
       alert("Goal Created!");
       onGoalAdded();
